b00f5f19eb207b3bf91114894e1234a510d711d7
Don <dmiesle@mac.com>
2018-08-26 15:12:08 -0400

    breakdown

--- src/fortuneteller.java
+++ src/fortuneteller.java
@@ -4,8 +4,6 @@ public class fortuneteller {
 
 	public static void main(String[] args) {
 //variables
-		String firstName = "";
-		String lastName = "";
 		double age = 0;
 		double birthMonth = 0;
 		String favoriteColor = "";
@@ -17,9 +15,44 @@ public class fortuneteller {
 		System.out.println("What is your birth month as a number (for example if you were born in March type 3?");
 		System.out.println("What is your favorite ROYGBIV color?");
 		System.out.println("How many siblings do you have?"); // trap for question what is a sibling
+//function core
+		
+		Scanner input = new Scanner (System.in);
+//first name
+		
+		System.out.println("What is your first name?");
+		String firstName = input.nextLine();
+		input.nextLine();
+		
+//last name
+		
+		System.out.println("What is your last name?");
+		String lastName = input.nextLine();
+		input.nextLine();
+		
+//age
+		System.out.println("What is your age?");
+		age = input.nextDouble();
+		input.nextLine();
+		
+//birth month - add catch for typed age and convert to number
+		System.out.println("What is your birth month as a number (for example if you were born in March type 3?");
+		birthMonth = input.nextDouble();
+		input.nextLine();
+		
+//Favorite color
+		System.out.println("What is your favorite ROYGBIV color?");
+		favoriteColor = input.nextLine();
+		input.nextLine();
+		
+//siblings
+		System.out.println("How many siblings do you have?");
+		numberSiblings = input.nextDouble();
+		
+//final return *[First Name]* *[Last Name]* will retire in *[# of years]* with *[bank balance]* in the bank, a vacation home in *[location]*, and travel by *[mode of transporation]*.
 
-//evaluation conditions
-
+		
+		input.close();
 	}
 
 }
