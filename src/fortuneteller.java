2830b2c6742e6925b60e12cd70560c031aba489d
Don <dmiesle@mac.com>
2018-08-26 14:56:03 -0400

    loading initial criteria

--- src/fortuneteller.java
+++ src/fortuneteller.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class fortuneteller {
 
 	public static void main(String[] args) {
+//variables
+		String firstName = "";
+		String lastName = "";
+		double age = 0;
+		double birthMonth = 0;
+		String favoriteColor = "";
+		double numberSiblings = 0;
+//questions
+		System.out.println("What is your first name?");
+		System.out.println("What is your last name?");
+		System.out.println("What is your age?");
+		System.out.println("What is your birth month as a number (for example if you were born in March type 3?");
+		System.out.println("What is your favorite ROYGBIV color?");
+		System.out.println("How many siblings do you have?"); // trap for question what is a sibling
 
+//evaluation conditions
 
 	}
 
