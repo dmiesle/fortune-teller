6367d95a1113a0c008c2711f88361006c46f2e67
Don <dmiesle@mac.com>
2018-10-07 16:14:53 -0400

    error trap correction

--- src/fortuneteller.java
+++ src/fortuneteller.java
@@ -39,15 +39,15 @@ public class fortuneteller {
 //Favorite color input
 		System.out.println("What is your favorite ROYGBIV color?");
 		String favoriteColor = input.nextLine();
-		while (!favoriteColor.equals("red") || !favoriteColor.equals("orange") || !favoriteColor.equals("yellow")
-				|| !favoriteColor.equals("green") || !favoriteColor.equals("blue") || !favoriteColor.equals("indigo")
-				|| !favoriteColor.equals("violet"))
-			if (favoriteColor.equals("Help") || favoriteColor.equals("help") || favoriteColor.equals("HELP")) {
+		while (!favoriteColor.equals("red") || !favoriteColor.equalsIgnoreCase("orange") || !favoriteColor.equalsIgnoreCase("yellow")
+				|| !favoriteColor.equalsIgnoreCase("green") || !favoriteColor.equalsIgnoreCase("blue") || !favoriteColor.equalsIgnoreCase("indigo")
+				|| !favoriteColor.equalsIgnoreCase("violet"))
+			if (favoriteColor.equalsIgnoreCase("help") {
 				System.out.println("The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.");
 				favoriteColor = input.nextLine();
-			} else if (favoriteColor.equals("red") || favoriteColor.equals("orange") || favoriteColor.equals("yellow")
-					|| favoriteColor.equals("green") || favoriteColor.equals("blue") || favoriteColor.equals("indigo")
-					|| favoriteColor.equals("violet"))
+			} else if (favoriteColor.equalsIgnoreCase("red") || favoriteColor.equalsIgnoreCase("orange") || favoriteColor.equalsIgnoreCase("yellow")
+					|| favoriteColor.equalsIgnoreCase("green") || favoriteColor.equalsIgnoreCase("blue") || favoriteColor.equalsIgnoreCase("indigo")
+					|| favoriteColor.equalsIgnoreCase("violet"))
 				break;
 			else {
 				System.out.println("You need to enter your favorite ROYGBIV color, not something else.");
